88c2dfc6b24b32022296b0e168a984a2d3c00f13
Dreaming-future <61195303+Dreaming-future@users.noreply.github.com>
2021-05-05 17:24:27 +0800

    wordcloud_project

---
+++ wordcloud_project/wordcloud/test.py
@@ -0,0 +1,9 @@
+import stylecloud
+
+stylecloud.gen_stylecloud(file_path='constitution.txt',
+                          icon_name='fas fa-football-ball',
+                          #palette='colorbrewer.sequential.Greens_5',
+                          #background_color='black',
+                          #pro_icon_path='fa-solid-900.ttf',
+                          #pro_css_path='fontawesome.min.css'
+                          )
